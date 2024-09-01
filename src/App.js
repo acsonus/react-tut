45a4e6c2de019f79a817d5f96e06c6d46be23c24
acsonus <80101919+acsonus@users.noreply.github.com>
2024-09-01 23:45:02 +0300

    this is made on web version of vscode and pushed in the cloud

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id="fuckemall" > 
+          </div>
       </header>
       <div>
         <D3chart />
